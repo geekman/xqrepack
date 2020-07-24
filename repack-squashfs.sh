d2fcf3bca9ac85a155e4ea19f9221c56a62d2fc4
Darell Tan <darell.tan@gmail.com>
2020-07-24 23:48:53 +0800

    Disable WAN check.
    Periodically checks for Internet connectivity through the
    check_wan_status.lua script by either pinging or resolving some fixed
    set of China domains like baidu, qq, tabao and its own api.miwifi.com.
    
    The downside of removing this is that the LED indicator on the router
    will not be accurate, but I don't mind that.

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -52,7 +52,9 @@ for SVC in stat_points statisticsservice \
 done
 
 # prevent stats phone home & auto-update
-for f in StatPoints mtd_crash_log logupload.lua otapredownload; do > $FSDIR/usr/sbin/$f; done
+for f in StatPoints mtd_crash_log logupload.lua otapredownload wanip_check.sh; do > $FSDIR/usr/sbin/$f; done
+
+rm -f $FSDIR/etc/hotplug.d/iface/*wanip_check
 
 sed -i '/start_service(/a return 0' $FSDIR/etc/init.d/messagingagent.sh
 
